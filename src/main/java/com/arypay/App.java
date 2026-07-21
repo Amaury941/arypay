6cafac61211890f09a127e0420ba9fb5c05eea44
Amaury <amaurylcjunior@gmail.com>
2026-07-20 22:59:50 -0300

    chore: fixing app.js

--- src/main/java/com/arypay/App.java
+++ src/main/java/com/arypay/App.java
@@ -1,10 +1,13 @@
 package com.arypay;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main (String[] argv) {
+        SpringApplication.run(App.class,argv);
     }
 }
