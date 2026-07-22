4c1e6892d327ca845db076ad1937c9f48e4c684a
Amaury <amaurylcjunior@gmail.com>
2026-07-21 23:48:23 -0300

    feat: added entity wallet

---
+++ src/main/java/com/arypay/wallet/Wallet.java
@@ -0,0 +1,29 @@
+package com.arypay.wallet;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "wallets")
+public class Wallet {
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private BigDecimal balance;
+    @Column(name = "user_id")
+    private String user;
+}
